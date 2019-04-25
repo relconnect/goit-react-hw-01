a478a9173a8740a82beac8bdafa9a30c49dad09a
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2019-04-25 15:02:30 +0300

    Updates

---
+++ precache-manifest.d23c2d0dba53691d6d04c2edb3d6408d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f51632aec18d5636ad74",
+    "url": "/goit-react-hw-01/static/js/runtime~main.bdf556df.js"
+  },
+  {
+    "revision": "9ac7d8b4031dccc80878",
+    "url": "/goit-react-hw-01/static/js/main.e85b99be.chunk.js"
+  },
+  {
+    "revision": "379bc3bb2177be902126",
+    "url": "/goit-react-hw-01/static/js/2.b49b47cf.chunk.js"
+  },
+  {
+    "revision": "9ac7d8b4031dccc80878",
+    "url": "/goit-react-hw-01/static/css/main.5934ce3b.chunk.css"
+  },
+  {
+    "revision": "293d9e0f4aaae05a25f7e62acddc010f",
+    "url": "/goit-react-hw-01/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.158395229c5e58431f3f65e87c4af09c.js"
+  "/goit-react-hw-01/precache-manifest.d23c2d0dba53691d6d04c2edb3d6408d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/goit-react-hw-01/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
