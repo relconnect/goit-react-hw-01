d9e3d83e73ea5223e7d154939badb3178b2a546f
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2019-04-25 14:52:50 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.220a20a579f1e80c1ea0f45f33616593.js"
+  "/precache-manifest.158395229c5e58431f3f65e87c4af09c.js"
 );
 
 workbox.clientsClaim();
